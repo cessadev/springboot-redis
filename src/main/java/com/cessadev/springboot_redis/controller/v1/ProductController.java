624cb6d7f183bb309d0d3fba5a9d4d7126864a1f
cessadev <cesarurb.dev@gmail.com>
2025-05-16 12:31:02 -0500

    v1.2.0

--- src/main/java/com/cessadev/springboot_redis/controller/v1/ProductController.java
+++ src/main/java/com/cessadev/springboot_redis/controller/v1/ProductController.java
@@ -1,11 +1,13 @@
 package com.cessadev.springboot_redis.controller.v1;
 
+import com.cessadev.springboot_redis.model.Product;
 import com.cessadev.springboot_redis.service.ProductService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("api/v1/products")
@@ -19,6 +21,29 @@ public class ProductController {
     try {
       return ResponseEntity.ok(productService.getAllProducts());
     } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @GetMapping("{productId}")
+  public ResponseEntity<?> getProductById(
+          @Valid @PathVariable Long productId) {
+    try {
+      return ResponseEntity.ok(productService.getProductById(productId));
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @PostMapping("/create")
+  public ResponseEntity<?> createProduct(
+          @Valid @RequestBody Product product) {
+    try {
+      Product productSaved = productService.createProduct(product);
+      return ResponseEntity
+              .created(URI.create("/api/v1/products/create"))
+              .body(productSaved);
+    } catch (RuntimeException e) {
       throw new RuntimeException(e);
     }
   }
